3d9f0ee6cd88d50412e960ab7e3fc32c4dfb4a78
thomasisa1 <thomasisa1@users.noreply.github.com>
2024-04-23 03:48:36 +0000

    domainGenerator for sonsole output

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronouns = ["the", "our", "my"];
+let adjectives = ["great", "big", "little"];
+let nouns = ["jogger", "racoon", "fox", "bear"];
+let extensions = [".com", ".net", ".org"];
+
+// Function to generate domain names
+function generateDomains(pronouns, adjectives, nouns, extensions) {
+  let domains = [];
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjectives.length; j++) {
+      for (let k = 0; k < nouns.length; k++) {
+        for (let ext = 0; ext < extensions.length; ext++) {
+          let domain = pronouns[i] + adjectives[j] + nouns[k] + extensions[ext];
+          domains.push(domain);
+        }
+      }
+    }
+  }
+  return domains;
+}
+
+let domains = generateDomains(pronouns, adjectives, nouns, extensions);
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  for (let i = 0; i < domains.length; i++) {
+    console.log(domains[i]);
+  }
 };
